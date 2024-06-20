919ab302c57f55fc448bfceac48eed26f2b4bce0
LCS <LCS@ANASBURWAIS>
2024-06-21 01:21:17 +0300

    Test All Methods Of Service

---
+++ ToDo.Tests/IntegrationTests/TaskServiceTests.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using ToDo.Domain.Entities;
+using ToDo.Persistance;
+using ToDo.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ToDo.Tests.IntegrationTests
+{
+    public class TaskServiceTests
+    {
+
+        private readonly IDbContextFactory<ToDoContext> _factory;
+
+        // الهدف هو ضمان أن كل مرة يتم فيها استدعاء الدالة، يتم إنشاء قاعدة
+        // بيانات جديدة وفريدة في الذاكرة للاستخدام في الاختبارات أو لأغراض أخرى
+        private DbContextOptions<ToDoContext> CreateNewContextOptions()
+        {
+            return new DbContextOptionsBuilder<ToDoContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        private IDbContextFactory<ToDoContext> GetDbContextFactoryAsync(DbContextOptions<ToDoContext> options)
+        {
+            var mockDbFactory = new Mock<IDbContextFactory<ToDoContext>>();
+            mockDbFactory.Setup(f => f.CreateDbContext()).Returns(() => new ToDoContext(options));
+            return mockDbFactory.Object;
+
+        }
+
+
+        // اختبار دالة الاضافة
+        [Fact]
+        public async Task AddTask_ShouldAddTask()
+        {
+            // Arrange // تجهيز الاختبار
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new TaskService(factory);
+           
+            var task = new Tasks { TaskName = "Task1", Ststus = false  };
+
+            // Act // استدعاء الدالة
+            await service.AddTask(task);
+
+            // Assert // التأكد
+            using var context = new ToDoContext(options);
+            var savedTask = await context.Task.FirstOrDefaultAsync(a => a.TaskName == "Task1");
+            Assert.NotNull(savedTask);
+        }
+
+        // اختبار دالة حذف
+        [Fact]
+        public async Task Delete_ShouldRemoveTask()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new TaskService(factory);
+            var task = new Tasks { TaskName = "Task1", Ststus = false };
+            await service.AddTask(task);
+
+            // Act
+            await service.DeleteTask(task.TaskId);
+
+            // Assert
+            using var context = new ToDoContext(options);
+            var deletedAuthor = await context.Task.FindAsync(task.TaskId);
+            Assert.Null(deletedAuthor);
+        }
+
+
+        // اختبار دالة إرجاع جميع المهام
+        [Fact]
+     public async Task GetAllTasks_ShouldReturnAllTasks()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new TaskService(factory);
+            await service.AddTask(new Tasks { TaskName = "Task1", Ststus = false });
+            await service.AddTask(new Tasks { TaskName = "Task2", Ststus = false });
+
+            // Act
+            var authors = await service.GetAllTasks();
+
+            // Assert
+            Assert.Equal(2, authors.Count);
+        }
+
+        // اختبار دالة إرجاع مهمة بالمعرف
+        [Fact]
+        public async Task GetTaskById_ShouldReturnTaskById()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new TaskService(factory);
+            var task = new Tasks { TaskName = "Task1", Ststus = false };
+            await service.AddTask(task);
+
+            // Act
+            var fetchedTask = await service.GetTaskById(task.TaskId);
+
+            // Assert
+            Assert.NotNull(fetchedTask);
+            Assert.Equal(task.TaskName, fetchedTask.TaskName);
+        }
+
+        // اختبار دالة التعديل
+        [Fact]
+        public async Task UpdateTask_ShouldModifyTask()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new TaskService(factory);
+            var task = new Tasks { TaskName = "Task1", Ststus = false };
+            await service.AddTask(task);
+
+            // Act
+            task.TaskName = "Updated Task1";
+            task.Ststus = false;
+            
+            await service.UpdateTask(task);
+
+            // Assert
+            using var context = new ToDoContext(options);
+            var updatedTask = await context.Task.FindAsync(task.TaskId);
+            Assert.Equal("Updated Task1", updatedTask.TaskName);
+            Assert.Equal(false, updatedTask.Ststus);
+        
+        }
+
+    }
+
+}
---
+++ ToDo.Tests/IntegrationTests/UserServiceTests.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using ToDo.Domain.Entities;
+using ToDo.Persistance;
+using ToDo.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ToDo.Tests.IntegrationTests
+{
+    public class UserServiceTests
+    {
+
+        private readonly IDbContextFactory<ToDoContext> _factory;
+
+        // الهدف هو ضمان أن كل مرة يتم فيها استدعاء الدالة، يتم إنشاء قاعدة
+        // بيانات جديدة وفريدة في الذاكرة للاستخدام في الاختبارات أو لأغراض أخرى
+        private DbContextOptions<ToDoContext> CreateNewContextOptions()
+        {
+            return new DbContextOptionsBuilder<ToDoContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        private IDbContextFactory<ToDoContext> GetDbContextFactoryAsync(DbContextOptions<ToDoContext> options)
+        {
+            var mockDbFactory = new Mock<IDbContextFactory<ToDoContext>>();
+            mockDbFactory.Setup(f => f.CreateDbContext()).Returns(() => new ToDoContext(options));
+            return mockDbFactory.Object;
+
+        }
+
+        // اختبار دالة الاضافة
+        [Fact]
+        public async Task AddUser_ShouldAddUser()
+        {
+            // Arrange // تجهيز الاختبار
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new UserService(factory);
+
+            var user = new User { UserName = "AnasBurwais", Password = "123", Email = "burwais@gmail.com"};
+
+            // Act // استدعاء الدالة
+            await service.AddUser(user);
+
+            // Assert // التأكد
+            using var context = new ToDoContext(options);
+            var savedUser = await context.User.FirstOrDefaultAsync(a => a.UserName == "AnasBurwais");
+            Assert.NotNull(savedUser);
+        }
+
+        // اختبار دالة تعديل المستخدم
+        [Fact]
+        public async Task UpdateUser_ShouldModifyUser()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new UserService(factory);
+            var user = new User { UserName = "AnasBurwais", Password = "123", Email = "burwais@gmail.com" };
+            await service.AddUser(user);
+
+            // Act
+            user.UserName = "Updated AnasBurwais";
+            user.Password = "Updated 123";
+            user.Email = "Updated burwais@gmail.com";
+
+            await service.UpdateUser(user);
+
+            // Assert
+            using var context = new ToDoContext(options);
+            var updatedUser = await context.User.FindAsync(user.UserId);
+            Assert.Equal("Updated AnasBurwais", updatedUser.UserName);
+            Assert.Equal("Updated 123", updatedUser.Password);
+            //Assert.Equal("Updated burwais@gmail.com", updatedUser.Email);
+
+
+        }
+
+        // اختبار دالة حذف
+        [Fact]
+        public async Task DeleteUser_ShouldRemoveUser()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new UserService(factory);
+            var user = new User { UserName = "AnasBurwais", Password = "123", Email = "burwais@gmail.com" };
+            await service.AddUser(user);
+
+            // Act
+            await service.DeleteUser(user.UserId);
+
+            // Assert
+            using var context = new ToDoContext(options);
+            var deletedUser = await context.Task.FindAsync(user.UserId);
+            Assert.Null(deletedUser);
+        }
+
+
+
+        // اختبار دالة إرجاع مستخدم بالمعرف
+        [Fact]
+        public async Task GetById_ShouldReturnUserById()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new UserService(factory);
+            var user = new User { UserName = "AnasBurwais", Password = "123", Email = "burwais@gmail.com" };
+            await service.AddUser(user);
+
+            // Act
+            var fetchedUser = await service.GetById(user.UserId);
+
+            // Assert
+            Assert.NotNull(fetchedUser);
+            Assert.Equal(user.UserName, fetchedUser.UserName);
+        }
+
+
+        // اختبار دالة إرجاع جميع المستخدمين
+        [Fact]
+        public async Task GetAllUsers_ShouldReturnAllUsers()
+        {
+            // Arrange
+            var options = CreateNewContextOptions();
+            var factory = GetDbContextFactoryAsync(options);
+            var service = new UserService(factory);
+            await service.AddUser(new User { UserName = "AnasBurwais", Password = "123", Email = "burwais@gmail.com" });
+            await service.AddUser(new User { UserName = "AnasBurwais2", Password = "1234", Email = "burwais2@gmail.com" });
+            // Act
+            var users = await service.GetAllUsers();
+
+            // Assert
+            Assert.Equal(2, users.Count);
+        }
+    }
+}
